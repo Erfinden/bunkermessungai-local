2a24b22fef1d248f3d48a4962b9b795697b2efa9
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-09-08 12:08:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,38 @@ sudo sed -i 's/#host-name=.*$/host-name=bunkermessungai/' /etc/avahi/avahi-daemo
 sudo systemctl restart avahi-daemon
 
 # Install required Python packages
-pip install Flask requests
+pip install Flask requests git
+
+# Clone the repo
+git clone https://github.com/Erfinden/bunkermessungai.local /home
+
+# Define the path to the cam.py script and the desired service name
+SCRIPT_PATH="/home/bunkermessungai.local/cam.py"
+SERVICE_NAME="cam"
+
+# Get the current user and group
+CURRENT_USER=$(whoami)
+CURRENT_GROUP=$(id -gn)
+
+# Create the systemd service unit file
+SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+echo "[Unit]" | sudo tee "$SERVICE_FILE" > /dev/null
+echo "Description=Run cam.py script" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "[Service]" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "ExecStart=/usr/bin/python3 $SCRIPT_PATH" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "Restart=always" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "User=$CURRENT_USER" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "Group=$CURRENT_GROUP" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "[Install]" | sudo tee -a "$SERVICE_FILE" > /dev/null
+echo "WantedBy=multi-user.target" | sudo tee -a "$SERVICE_FILE" > /dev/null
+
+# Reload systemd
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable "$SERVICE_NAME.service"
+sudo systemctl start "$SERVICE_NAME.service"
 
 echo "Installation completed successfully."
