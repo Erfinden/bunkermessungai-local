bc103483219be9440be7794689091cda8b2c65b1
nanocraftmr <mr.nanocrafterin2@gmail.com>
2023-10-12 21:33:05 +0200

    hdg added

--- cam.py
+++ cam.py
@@ -7,52 +7,72 @@ import platform
 import os
 import datetime
 
-main_url = "https://bunkermessungai.de:5000"
+cookies = {'access_token_cookie': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5NjY5NjA5OSwianRpIjoiMzE1YmU3NzItYzlhZC00ZGZkLTliYWUtYmUyY2IwZGJmMmE5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3QiLCJuYmYiOjE2OTY2OTYwOTksImV4cCI6MTY5NjY5Njk5OX0.STtFU-gxvgoIvsT9qY2dldnJVevBtyS_g8aRZrOSQn4'}
 
-try:
-    import RPi.GPIO as GPIO
-except ImportError:
-    print("GPIO library not installed. This code is intended to run on Raspberry Pi.")
+# try to import GPIO on linux
+if platform.system() == 'Linux':
+    try:
+        import RPI.GPIO
+        import RPi.GPIO as GPIO
+    except:
+        print("")
+        print("GPIO library not installed: pip install RPI.GPIO when on a rasperry pi, else ignore")
+        print("")
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
-PowerLED = 23
-StatusLED = 24
-GPIO.setup(PowerLED, GPIO.OUT)
-GPIO.setup(StatusLED, GPIO.OUT)
-GPIO.output(PowerLED, GPIO.HIGH)
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
+    PowerLED = 23
+    StatusLED = 24
+    GPIO.setup(PowerLED, GPIO.OUT)
+    GPIO.setup(StatusLED, GPIO.OUT)
+    GPIO.output(PowerLED, GPIO.HIGH)
+except:
+    pass
 
-with open('/home/bunkermessungai-local/config.json', 'r') as f:
-    config = json.load(f)
-
+main_url = config["main_url"]
 capturing = False
 capture_thread = None
 
 def capture_and_upload():
     global capturing
     while capturing:
-        with open('/home/bunkermessungai-local/config.json', 'r') as f:
+        with open('config.json', 'r') as f:
             config = json.load(f)
         try:
+            # Capture and upload image
             if platform.system() == 'Windows':
                 capture_image_windows(config['key'])
             elif platform.system() == 'Linux':
                 capture_image_linux(config['key'])
+            
+            # Fetch and print data for each IP address
+            fetch_data_from_brenner()
+            
+            # Sleep for the appropriate amount of time
             time.sleep(86400 / int(config['time']))
         except Exception as e:
             print("Error:", e)
 
-def upload_image(key):
+
+def upload_image(key, brenner_info):
+    global cookies
     url = f'{main_url}/upload'
     files = {'image': open("static/images/captured_image.jpg", 'rb')}
-    data = {'key': key}
+    data = {'key': key, 'brenner': brenner_info}
+    cookies=cookies
     try:
-        response = requests.post(url, files=files, data=data)
-        print(response.text)
-    except:
-        print("Error uploading Image")
+        response = requests.post(url, files=files, data=data, cookies=cookies)
+        print(response.json())
+    except Exception as e:
+        print("Error uploading Image. error: \n"+ e)
+
 
 def capture_image_windows(key):
     import cv2
@@ -99,18 +119,27 @@ def stop_capture():
 
 def ping_server(key):
     global config
+    global cookies
     url = f'{main_url}/status_cam'
     data = {'key': key}
+    cookies=cookies
     try:
-        response = requests.post(url, data=data)
-        response.raise_for_status()
+        response = requests.post(url, data=data,cookies=cookies)
         print(response.text)
         config['ping_success'] = True
-        GPIO.output(StatusLED, GPIO.HIGH)
+        if platform.system() == 'Linux':
+            try:
+                GPIO.output(StatusLED, GPIO.HIGH)
+            except:
+                pass
     except requests.RequestException as e:
         print(f"Error pinging the server: {e}")
         config['ping_success'] = False
-        GPIO.output(StatusLED, GPIO.LOW)
+        if platform.system() == 'Linux':
+            try:
+                GPIO.output(StatusLED, GPIO.LOW)
+            except:
+                pass
     except Exception as e:
         print(f"Unexpected error: {e}")
 
@@ -127,14 +156,86 @@ def update_config():
     time = request.form.get('time')
     config['key'] = key
     config['time'] = time
-    with open('/home/bunkermessungai-local/config.json', 'w') as f:
+    with open('config.json', 'w') as f:
         json.dump(config, f, indent=4)
     return redirect('/')
 
+
+class HdgComm:
+    def __init__(self, url, q):
+        if not url or not q:
+            return
+        self.base_url = f"http://{url}"
+        self.data_query = {'nodes': q}
+        self.headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+
+    def data_refresh(self, cb):
+        try:
+            response = requests.post(
+                f"{self.base_url}/ApiManager.php?action=dataRefresh",
+                data=self.data_query,
+                headers=self.headers,
+                timeout=5  # set a timeout for the request
+            )
+            if response.json():
+                return cb(response.json(), "")
+            else:
+                return cb("", "Keine Daten erhalten")
+        except requests.exceptions.ConnectTimeout:
+            return cb("", f"Verbindung zu {self.base_url} hat zu lange gedauert (Timeout)")
+        except requests.exceptions.RequestException as error:
+            return cb("", f"Fehler bei Anfrage an {self.base_url}: {str(error)}")
+        except Exception as e:
+            return cb("", f"Unerwarteter Fehler: {str(e)}")
+
+
+
+def callback(data, error, brenner_name, ip_adress):
+    brenner_data = []
+    if data:
+        for item in data:
+            if 'text' in item:
+                brenner_info = f"{brenner_name}, (Ip: {ip_adress}), {item['text']}"
+                brenner_data.append(brenner_info)
+                print(brenner_info)
+    else:
+        print("Fehler:", error)
+    
+    # Combine brenner data into a single string and upload
+    return "; ".join(brenner_data)
+
+
+
+def fetch_data_from_brenner():
+    q = "22069"  # Query parameter for data fetching
+    
+    # Fetch and print data for each IP address
+    all_brenner_data = []
+    for idx, brenner in enumerate(config['brenner'], start=1):
+        ip_address = brenner['ip']
+        brenner_name = brenner.get('name', f'brenner[{idx}]')
+        hdg_comm = HdgComm(ip_address, q)
+        
+        # Get brenner data
+        brenner_data = hdg_comm.data_refresh(lambda data, error: callback(data, error, brenner_name, ip_address))
+        all_brenner_data.append(brenner_data)
+
+    # Combine all brenner data into a single string and upload
+    upload_image(config['key'], "; ".join(all_brenner_data))
+
+
 if __name__ == '__main__':
     try:
+        # Start threads for periodic pinging and data fetching
         threading.Thread(target=periodic_ping).start()
+        # Run Flask
         app.run(host='0.0.0.0', port=80)
     except KeyboardInterrupt:
         print("Server stopped by user.")
-        GPIO.cleanup()
+        if platform.system() == 'Linux':
+            try:
+                GPIO.cleanup()
+            except:
+                pass
