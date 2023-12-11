308e075dbc90dac140ebe11820eea2e049e82c42
Nepomuk Esterhazy <128908840+nanocraftmr@users.noreply.github.com>
2023-12-11 23:27:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo sed -i 's/#host-name=.*$/host-name=bunkermessungai/' /etc/avahi/avahi-daemo
 sudo systemctl restart avahi-daemon
 
 # Install required Python packages
-sudo pip install flask requests RPi.GPIO
+sudo pip install flask requests RPi.GPIO cryptography
 
 # Clone the repo
 sudo git clone https://github.com/Erfinden/bunkermessungai-local /home/bunkermessungai-local
