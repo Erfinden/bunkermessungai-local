47038e193fd18562d1bf45aa61dca39cc230468f
nanocraftmr <mr.nanocrafterin2@gmail.com>
2024-02-16 17:48:39 +0100

    update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,64 +1,75 @@
 #!/bin/bash
+#!/bin/bash
+function install_raspi {
+    # Install Raspberrypi specific packages
+    sudo apt install -y libraspberrypi-bin
+    ###### Set Hostname to bunkermessungai.local ######
+    # Install Avahi Daemon
+    sudo apt install -y avahi-daemon
+
+    # Configure Avahi Daemon
+    sudo sed -i 's/#host-name=.*$/host-name=bunkermessungai/' /etc/avahi/avahi-daemon.conf
+    ###################################################
+
+
+    ###### Auto Run Script on Boot ######
+    # Define the path to the main.py script and the desired service name
+    SCRIPT_PATH="/home/bunkermessungai-local/main.py"
+    SERVICE_NAME="cam"
+
+    # Get the current user and group for the service
+    CURRENT_USER=$(whoami)
+    CURRENT_GROUP=$(id -gn)
+
+    # Create the systemd service unit file
+    SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+    sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+    [Unit]
+    Description=Run cam.py script
+
+    [Service]
+    ExecStart=/usr/bin/python3 $SCRIPT_PATH
+    Restart=always
+    User=$CURRENT_USER
+    Group=$CURRENT_GROUP
+
+    [Install]
+    WantedBy=multi-user.target
+EOF
+
+    # Restart Avahi Daemon
+    sudo systemctl restart avahi-daemon
+
+    # Reload systemd
+    sudo systemctl daemon-reload
+
+    # Enable and start the service
+    sudo systemctl enable "$SERVICE_NAME.service"
+    sudo systemctl start "$SERVICE_NAME.service"
+    #####################################
+}
+
 
 # Install Prerequisites
-sudo apt install -y python3-pip python3 git fswebcam libraspberrypi-bin
-
-# Enable camera module
-sudo raspi-config nonint do_camera 0
-
-# Install Avahi Daemon
-sudo apt install avahi-daemon
-
-# Configure Avahi Daemon
-sudo sed -i 's/#host-name=.*$/host-name=bunkermessungai/' /etc/avahi/avahi-daemon.conf
-
-# Install required Python packages
-sudo pip install -r ../requirements.txt
-
+sudo apt install -y python3-pip python3 git fswebcam
 # Clone the repo
 sudo git clone https://github.com/Erfinden/bunkermessungai-local /home/bunkermessungai-local
 
-
-# Define the path to the main.py script and the desired service name
-SCRIPT_PATH="/home/bunkermessungai-local/main.py"
-SERVICE_NAME="cam"
-
-# Get the current user and group for the service
-CURRENT_USER=$(whoami)
-CURRENT_GROUP=$(id -gn)
-
-# Create the systemd service unit file
-SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-sudo tee "$SERVICE_FILE" > /dev/null <<EOF
-[Unit]
-Description=Run cam.py script
-
-[Service]
-ExecStart=/usr/bin/python3 $SCRIPT_PATH
-Restart=always
-User=$CURRENT_USER
-Group=$CURRENT_GROUP
-
-[Install]
-WantedBy=multi-user.target
-EOF
+# Install required Python packages
+sudo pip install -r /home/bunkermessungai-local/requirements.txt
 
 
-# Restart Avahi Daemon
-sudo systemctl restart avahi-daemon
-
-# Reload systemd
-sudo systemctl daemon-reload
-
-# Enable and start the service
-sudo systemctl enable "$SERVICE_NAME.service"
-sudo systemctl start "$SERVICE_NAME.service"
+# Check if running on Raspberry Pi
+if [[ $(uname -m) == "arm"* ]]; then
+    echo "Running on Raspberry Pi, setting up Kamera, Hostname and Autostart."
+    install_raspi
+else
+    echo "Not running on Raspberry Pi, Skipping further setup."
+fi
 
 echo "Installation completed successfully."
-sleep 1
-echo "rebooting in ...3"
-sleep 1
-echo "rebooting in ...2"
-sleep 1
-echo "rebooting in ...1"
-sudo reboot
+echo "Reboot? (y/n)"
+read -r REBOOT
+if [[ "$REBOOT" == "y" ]]; then
+    sudo reboot
+fi
